2fc57905dce258584a5c428d79a6b21deb24bd11
Tomas Korcak <korczis@gmail.com>
2017-05-30 00:01:49 +0200

    Better handling of failed HackRF init in examples/rx_stream.rs

--- hackrf/examples/rx_stream.rs
+++ hackrf/examples/rx_stream.rs
@@ -8,7 +8,8 @@ fn main() {
 
     let _hrf = match HackRF::init() {
         Ok(hrf) => hrf,
-        _ => {
+        Err(err) => {
+            println!("Unable to init HackRF, reason: {:?}", err);
             return;
         }
     };
@@ -17,6 +18,13 @@ fn main() {
 
     let device = &mut devices[0];
 
-    device.set_sample_rate(2_000_000 as f64);
-    device.set_frequency(150_000_000);
+    if let Err(err) = device.set_sample_rate(2_000_000 as f64) {
+        println!("Unable to set_sample_rate, reason: {:?}", err);
+        return;
+    }
+
+    if let Err(err) = device.set_frequency(150_000_000) {
+        println!("Unable to set_frequency(), reason: {:?}", err);
+        return;
+    }
 }
