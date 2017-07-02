5a5ec0b5d7d2631559e7d1177267d352375f5b8d
Tomas Korcak <korczis@gmail.com>
2017-07-02 19:09:28 +0200

    Skeleton of elixir math implementation

---
+++ math-elixir/lib/math.ex
@@ -0,0 +1,18 @@
+defmodule Math do
+  @moduledoc """
+  Documentation for Math.
+  """
+
+  @doc """
+  Simple addition
+
+  ## Examples
+
+      iex> Math.add 1, 2
+      :world
+
+  """
+  def add(a, b) do
+    a + b
+  end
+end
